13df39865fc0dc2325fc8862bb8555d419682cef
Aster Boese <asterboese@mailbox.org>
2025-07-01 19:43:00 -0400

    */*: update to pass new shellcheck CI
    Signed-off-by: Aster Boese <asterboese@mailbox.org>

--- device/community/device-pine64-pinephone/setup-modem.sh
+++ device/community/device-pine64-pinephone/setup-modem.sh
@@ -14,9 +14,9 @@ else
 fi
 
 # Read current config
-QMBNCFG_ACTUAL_CONFIG=$(echo 'AT+QMBNCFG="AutoSel"' | atinout - $DEV -)
+QMBNCFG_ACTUAL_CONFIG=$(echo 'AT+QMBNCFG="AutoSel"' | atinout - "$DEV" -)
 
-if echo $QMBNCFG_ACTUAL_CONFIG | grep -q $QMBNCFG_CONFIG
+if echo "$QMBNCFG_ACTUAL_CONFIG" | grep -q $QMBNCFG_CONFIG
 then
 	log "Modem already configured"
 	exit 0
@@ -24,8 +24,8 @@ fi
 
 
 # Configure VoLTE auto selecting profile
-RET=$(echo "AT+QMBNCFG=\"AutoSel\",$QMBNCFG_CONFIG" | atinout - $DEV -)
-if ! echo $RET | grep -q OK
+RET=$(echo "AT+QMBNCFG=\"AutoSel\",$QMBNCFG_CONFIG" | atinout - "$DEV" -)
+if ! echo "$RET" | grep -q OK
 then
 	log "Failed to enable VoLTE profile auto selecting: $RET"
 	exit 1
